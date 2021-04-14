eed5feed8648f4f571dc45547c099352090f966d
Константин <skripkin_kostyunya@mail.ru>
2021-04-14 13:27:33 +0600

    Создал сервер и авторизацию

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,52 @@
 import React from 'react';
+import axios from 'axios';
+
+import socket from './socket';
 import './App.scss';
+import Reducer from './reducer';
 
 import { LogIn, ChatWindow, UsersSidebar } from "./components";
 
+const INITIAL_STATE = {
+  joined: false,
+  userName: null,
+  users: [],
+  messages: [],
+};
+
 function App() {
-  const [logedIn, setLogedIn] = React.useState(false);
+  const [state, dispatch] = React.useReducer((arg: any, action: any) => Reducer(arg, action), INITIAL_STATE);
+
+  const onLogin = async (obj: { userName: string }) => {
+    dispatch({
+      type: 'JOINED',
+      payload: obj,
+    });
+    socket.emit('ROOM:JOIN', obj);
+    const { data } = await axios.get(`/rooms`);
+    dispatch({
+      type: 'SET_DATA',
+      payload: data,
+    });
+  };
+
+  const setUsers = (users: string[]) => {
+    dispatch({
+      type: 'SET_USERS',
+      payload: users,
+    });
+  };
+
+  React.useEffect(() => {
+    socket.on('ROOM:SET_USERS', setUsers);
+  }, []);
+
+  console.log(state)
 
   return (
     <div className="wrapper">
-      {!logedIn
-        ? <LogIn onLogin={({ userName }) => setLogedIn(true)} />
+      {!state.joined
+        ? <LogIn onLogin={onLogin} />
         : <div className="chat">
           <ChatWindow
             messages={[
@@ -22,7 +59,7 @@ function App() {
             userName="Dena"
             onAddMessage={({ userName, text }) => null}
           />
-          <UsersSidebar users={["Jone", "Dena"]} />
+          <UsersSidebar users={state.users} />
         </div>
       }
     </div>
---
+++ src/reducer.js
@@ -0,0 +1,27 @@
+const Reducer = (state, action) => {
+  switch (action.type) {
+    case 'JOINED':
+      return {
+        ...state,
+        joined: true,
+        userName: action.payload.userName,
+      };
+
+    case 'SET_DATA':
+      return {
+        ...state,
+        users: action.payload.users,
+      };
+
+    case 'SET_USERS':
+      return {
+        ...state,
+        users: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default Reducer;
